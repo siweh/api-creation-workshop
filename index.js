8a5937b1ee978872710b3b3e795f4682a9860e10
siweh <sanemadesi@gmail.com>
2022-04-01 15:08:49 +0200

    Added the filtering and it's working properly

--- index.js
+++ index.js
@@ -1,1 +1,56 @@
-// add code in here to create an API with ExpressJS
+//? add code in here to create an API with ExpressJS
+const express = require('express');
+// import the dataset to be used here
+//!This is a list of garments that we will be using as data for our API.
+const garments = require('./garments.json');
+const app = express();
+
+//?Enabling the static folder
+app.use(express.static('public'));
+
+//?Import the port number to be used here
+const PORT = process.env.PORT || 4017;
+
+app.get('/api/garments', function(req, res){
+
+	const gender = req.query.gender;
+	const season = req.query.season;
+
+	const filteredGarments = garments.filter(garment => {
+		// if both gender & season was supplied
+		if (gender != 'All' && season != 'All') {
+			return garment.gender === gender 
+				&& garment.season === season;
+		} else if(gender != 'All') { // if gender was supplied
+			return garment.gender === gender
+		} else if(season != 'All') { // if season was supplied
+			return garment.season === season
+		}
+		return true;
+	});
+
+	// note that this route just send JSON data to the browser
+	// there is no template
+	res.json({ 
+		garments : filteredGarments
+	});
+});
+
+app.get('/api/garments/price/:price', function(req, res){
+	const maxPrice = Number(req.params.price);
+	const filteredGarments = garments.filter( garment => {
+		// filter only if the maxPrice is bigger than maxPrice
+		if (maxPrice > 0) {
+			return garment.price <= maxPrice;
+		}
+		return true;
+	});
+
+	res.json({ 
+		garments : filteredGarments
+	});
+});
+
+app.listen(PORT, function(){
+    console.log(`App started on port ${PORT}`)
+});
